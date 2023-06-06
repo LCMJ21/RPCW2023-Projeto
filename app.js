441267bc3bf7c649437edbcc5342d1bacb33bbf3
“Pierini10” <pierini10@gmail.com>
2023-06-07 00:49:51 +0100

    Add acordaos model (not completed)

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var mongoDB = 'mongodb://127.0.0.1/TPteste';
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', function () {
+    console.log('Erro de conexão ao MongoDB...');
+});
+db.on('open', function () {
+    console.log('Conexão ao MongoDB realizada com sucesso...');
+});
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,7 +23,10 @@ var app = express();
 
 // Bootstrap path includes
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/icons', express.static(__dirname + '/node_modules/bootstrap-icons/font'));
+app.use(
+    '/icons',
+    express.static(__dirname + '/node_modules/bootstrap-icons/font')
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,19 +42,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
