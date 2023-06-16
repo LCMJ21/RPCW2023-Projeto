05d92c8c37fe4dfb91cdc63bc5c809d657702073
sucodlaranja <joaoandre210@gmail.com>
2023-06-16 15:10:35 +0100

    removed unused import

--- controllers/accordion.js
+++ controllers/accordion.js
@@ -1,6 +1,5 @@
 const accordion = require("../models/acordao");
 const pagination = require("./pagination");
-var ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports.list = (page, perPage) => {
   return pagination.paginatedResults(accordion, page, perPage);
